2ab0b974a373a6a2361fc6a20a15e0d106b53d31
deces123 <trlghieu@gmail.com>
2026-06-09 10:10:38 +0200

    Update filter_scoary_trait.sh

--- RBP_target_search_in_Eco/filter_scoary_trait.sh
+++ RBP_target_search_in_Eco/filter_scoary_trait.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+### Edit here for the file path of your input scoary folder, and your output tsv
 BASE="/home/deces123/Project/Vin_Phage/bakta/scoary_Ecoli/scoary"
 OUT="${BASE}/significant_genes.tsv"
 
 header_written=0
 > "$OUT"
 
+### Edit the two 0.05 values below to your desired threshold for best_fisher_q and best_empirical_p of the scoary traits
 awk -F'\t' '
 NR > 1 && $3 < 0.05 && $4 < 0.05 {
     print $1
@@ -36,7 +38,7 @@ NR > 1 && $3 < 0.05 && $4 < 0.05 {
         }
         next
     }
-
+### Edit the two 0.05 values below to your desired threshold for best_fisher_q and best_empirical_p of the genes in each trait
     $fq < 0.05 && $ep < 0.05 {
         print trait, $0
     }
